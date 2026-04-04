b31146bf8421676cab59d56441ec0a2a49474b4b
Mark Yacoub <Mark-Atef@users.noreply.github.com>
2026-04-04 22:11:21 +0200

    Added Order Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import Pizza from './Pizza';
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's Order Now</h1>
-      <Pizza
-        name="The Pepperoni Pizza"
-        description="Mozzarella cheese, pepperoni"
-        image="http://localhost:3000/public/pizzas/pepperoni.webp"
-      />
-      <Pizza name="The Hawaiian Pizza" description="Mozzarella cheese, ham, pineapple" image="/public/pizzas/hawaiian.webp" />
-      <Pizza name="The Meat Lovers Pizza" description="Mozzarella cheese, pepperoni, sausage, bacon" image="/public/pizzas/big_meat.webp" />
+      <Order />
     </div>
   );
 };
